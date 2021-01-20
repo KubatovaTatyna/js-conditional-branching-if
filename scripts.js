abd18c3c93a0ed2dadea9c5f1305734186e9f973
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-01-20 21:07:44 +0600

    Js question-1 about A.S.Pushkin

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let result = 0;
+// Question 1
+let question1 = prompt ("When was born A.S. Pushkin?");
+if (question1 === 1799){
+    result += 5;
+}
+
+
+
+
+
+
+alert("You've earned " + result + " points")
