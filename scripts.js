d70dba097277fc5f678ef86cf643451f3cda5a80
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-01-21 22:18:19 +0600

    3d example about continents

--- scripts.js
+++ scripts.js
@@ -4,11 +4,26 @@ let question1 = prompt ("When was born A.S. Pushkin?");
 if (question1 === 1799){
     result += 5;
 }
+else if (!question1){
+    result = 0;
+}
 // Question 2
 let question2 = prompt ("Who opened the table of chemical elements?");
 if (question2 === "Mendeleev"){
     result += 5;
 }
+else if(!question2){
+    result = 0;
+}
+// Question 3
+let question3 = prompt ("How many continents on the planet?");
+if (question3 === 6){
+    result += 5;
+}
+else if (!question3){
+    result = 0;
+}
+
 
 
 
